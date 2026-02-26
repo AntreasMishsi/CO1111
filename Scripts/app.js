37746ee03fbcc8e610e1ea96e775d708a08c2db2
mandreou5 <mandreou5@uclan.ac.uk>
2026-02-26 14:30:45 +0200

    Update app.js
    MCQuestion added

--- Scripts/app.js
+++ Scripts/app.js
@@ -549,7 +549,57 @@ class TEXTquestion extends Question {
     }
 }
 
+class MCQuestion extends Question {
 
+
+    constructor(props) {
+        super(props);
+    }
+
+    Display(parentId) {
+        const container = document.getElementById(parentId);
+
+        // Render the form with radio buttons
+        container.innerHTML = `
+            <form id="mcqForm">
+                <p>${this.questionText}</p>
+                <input type="radio" id="A" name="mcq_question" value="A">
+                <label for="A">A</label><br>
+                <input type="radio" id="B" name="mcq_question" value="B">
+                <label for="B">B</label><br>
+                <input type="radio" id="C" name="mcq_question" value="C">
+                <label for="C">C</label><br>
+                <input type="radio" id="D" name="mcq_question" value="D">
+                <label for="D">D</label><br>
+                <button type="button" id="submitAnswer">Submit</button>
+            </form>
+        `;
+
+        // Add click listener for the button to get the answer
+        const submitButton = document.getElementById("submitAnswer");
+        submitButton.addEventListener("click", () => {
+            const selected = document.querySelector('input[name="mcq_question"]:checked');
+            this.Answear(selected.value);
+        });
+    }  
+   
+
+    async Answear(answear) {
+        const API_URL_ANSWER = `https://codecyprus.org/th/api/answer?session=${app.session}&answer=${answear}`;
+        const data = fetchData(API_URL_ANSWER).then(data => {
+            console.log(data.correct);
+            if(data.correct == false) {
+                playWrongAnimation();
+            }
+            else {
+                playCorrectAnimation();
+            }
+           
+        });
+        await sleep(animationDuration);
+        this.parentStage.AskQuestion();
+    }
+}
 
 //endquestions
 function sleep(ms) {
