7418111a84745fab77e06cdf9367335972842f56
AntreasMishis <amishis@uclan.ac.uk>
2026-03-22 22:46:40 +0200

    Update IntegerQuestion.js

--- Scripts/Question/IntegerQuestion.js
+++ Scripts/Question/IntegerQuestion.js
@@ -1,35 +1,47 @@
-import { nextDot } from '../progress-dots.js';
 import { app } from "../App/App.js";
+
 import { Question } from "./Question.js";
+
 import { fetchData } from "../Utils/Utils.js";
-import { playCorrectAnimation, playWrongAnimation, animationDuration, FadeOut } from "../Animations/AfterQuestionAnims.js";
+
+import { playCorrectAnimation, playWrongAnimation, animationDuration, FadeOut, FADE_OUT_DURATION} from "../Animations/AfterQuestionAnims.js";
+
+import { RENDERED_AREA_ID } from "../Utils/ClearRenderer.js";
+
 import { sleep } from "../Utils/Utils.js";
 import { Message } from "../Utils/Message.js";
 
 export class IntegerQuestion extends Question {
+
+
     constructor(props) {
         super(props);
     }
 
     Display(parentId) {
+        const API_SCORE = `https://codecyprus.org/th/api/score?session=${app.session}`;
         const container = document.getElementById(parentId);
+
+
+
+
+        // Render the form with radio buttons
         container.innerHTML = `
-            <div class="question-card">
-                <p class="question-text">${this.questionText}</p>
-                <input class="question-input" type="number" id="integerInput" placeholder="Enter an integer number" step="1" oninput="this.value = Math.round(this.value);">
-                <div class="question-buttons">
-                    <button type="button" id="submitAnswer" class="btn-submit">Submit</button>
-                    ${this.canBeSkipped ? `<button type="button" id="skipButton" class="btn-skip">Skip</button>` : ''}
-                </div>
+            <h2>Score: ${app.score}</h2>
+            <div id="integerForm">
+                <p>${this.questionText}</p>
+                <input type="number" id="integerInput" name="integer_question" placeholder="Enter an integer number" step="1" oninput="this.value = Math.round(this.value);">
+                <button type="button" id="submitAnswer">Submit</button>
+                ${this.canBeSkipped ? `<button type="button" id="skipButton">Skip</button>` : ''}
             </div>
         `;
 
-        document.getElementById("submitAnswer").addEventListener("click", () => {
+        // Add click listener for the button to get the answer
+        const submitButton = document.getElementById("submitAnswer");
+        submitButton.addEventListener("click", () => {
             const input = document.getElementById("integerInput").value;
-            const number = parseInt(input, 10);
+            const number = parseInt(input, 10); // Convert string to integer
             if (!isNaN(number)) {
-                //disable the button, in order to avoid repeiting request during fade-out animation
-                submitButton.disabled = true;
                 this.Answear(number); // Pass the integer to the Answer method
             } else {
                 const tmpMSG = new Message("Please enter a valid integer.");
@@ -46,19 +58,25 @@ export class IntegerQuestion extends Question {
         }
     }
 
+
     async Answear(answear) {
         const API_URL_ANSWER = `https://codecyprus.org/th/api/answer?session=${app.session}&answer=${answear}`;
+
+        // Promise that we will get the data
         const dataPromise = fetchData(API_URL_ANSWER);
+
+        // start the animation
         await FadeOut();
+
+        // wait till we get the data
         const data = await dataPromise;
+
         if (data.correct == false) {
             playWrongAnimation();
-        }
-        else
-        {
+        } else {
             playCorrectAnimation();
-            nextDot();
         }
+
         await sleep(animationDuration);
         this.parentStage.AskQuestion();
     }
