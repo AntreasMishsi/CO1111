38e133710391267b64f35320cbde42e4b971c8b8
AntreasMishis <amishis@uclan.ac.uk>
2026-03-22 22:29:59 +0200

    Update AfterQuestionAnims.js

--- Scripts/Animations/AfterQuestionAnims.js
+++ Scripts/Animations/AfterQuestionAnims.js
@@ -1,35 +1,31 @@
-
-
 import { ClearRenderer, RENDERED_AREA_ID } from "../Utils/ClearRenderer.js";
 
-export const animationDuration = 1000;
+import { sleep } from "../Utils/Utils.js";
 
+
+//users A, B, C didnt like the rotatitating animation, 
+//users A, B found animations slow
+// user C found a bag that changed animation from correct to wrong, it was due to fact that submit button could be pressed multiple times during fade out
+export const animationDuration = 1500;
+
+export const FADE_IN_DURATION = 500;
+export const FADE_OUT_DURATION = 500;
 
 export function playCorrectAnimation() {
+
     const container = document.getElementById(RENDERED_AREA_ID);
     ClearRenderer();
     // Create a green checkmark
     const check = document.createElement("div");
-    check.innerHTML = "✔";
-    check.style.position = "absolute";
+    check.className = "correct-animation-container";
+    check.innerHTML = `<img src=../Resources/icons/check.svg alt="correct icon" style="width:300px; height: 300px;">`;
     check.style.fontSize = "50px";
     check.style.color = "green";
-    check.style.opacity = "0";
-    check.style.transition = "all 0.5s ease-out";
     container.appendChild(check);
 
-    // Animate the checkmark
-    requestAnimationFrame(() => {
-        check.style.opacity = "1";
-        check.style.transform = "scale(1.5)";
-    });
 
-    // Fade out and remove after 1 second
-    setTimeout(() => {
-        check.style.opacity = "0";
-        check.style.transform = "scale(1)";
-        setTimeout(() => container.removeChild(check), 500);
-    }, animationDuration);
+
+
 }
 
 
@@ -38,24 +34,30 @@ export function playWrongAnimation() {
     ClearRenderer();
     // Create a red x
     const check = document.createElement("div");
-    check.innerHTML = "X";
-    check.style.position = "absolute";
-    check.style.fontSize = "50px";
-    check.style.color = "red";
-    check.style.opacity = "0";
-    check.style.transition = "all 0.5s ease-out";
+    check.className = "wrong-animation-container";
+    check.innerHTML = `<img src="Resources/icons/x.png" alt="correct icon" style="width:300px; height:300px;">`;
     container.appendChild(check);
 
     // Animate the checkmark
-    requestAnimationFrame(() => {
-        check.style.opacity = "1";
-        check.style.transform = "scale(1.5)";
-    });
+    
 
     // Fade out and remove after 1 second
     setTimeout(() => {
-        check.style.opacity = "0";
-        check.style.transform = "scale(1)";
-        setTimeout(() => container.removeChild(check), 500);
+        container.removeChild(check);
     }, animationDuration);
+}
+
+
+export async function FadeIn() {
+    document.getElementById(RENDERED_AREA_ID).classList.add("fade-in");
+    document.getElementById(RENDERED_AREA_ID).classList.remove("fade-out");
+    await sleep(FADE_IN_DURATION);
+    document.getElementById(RENDERED_AREA_ID).classList.remove("fade-in");
+}
+
+export async function FadeOut() {
+    document.getElementById(RENDERED_AREA_ID).classList.add("fade-out");
+    document.getElementById(RENDERED_AREA_ID).classList.remove("fade-in");
+    await sleep(FADE_OUT_DURATION);
+    document.getElementById(RENDERED_AREA_ID).classList.remove("fade-out");
 }
