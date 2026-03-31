6b235c4d4bc13f2a7fe354dfa1db663e1b7d7c3b
Andreas1117-dot <AGeorgiou10@uclan.ac.uk>
2026-03-31 11:43:54 +0300

    Improve comments and fix typos in Scanner.js
    Refactor comments for clarity and fix typos in Scanner.js.

--- Scripts/Utils/Scanner.js
+++ Scripts/Utils/Scanner.js
@@ -1,14 +1,21 @@
 import { Message } from "./Message.js";
 // v
 let scanner = null;
+
+//currently active camera
 let currentCamera = 0;
 
+// array for cameras
 let cameras = [];
 
+
+//opens the scanner or closes it if its already ative
 export function OpenScanner() {
 
+//video preview
     document.getElementById('preview').classList.add('active');
 
+// if its already active it closes it
     if (scanner) {
         scanner.stop();
         scanner = null;
@@ -16,7 +23,7 @@ export function OpenScanner() {
         return;
     }
 
-    // scanner options
+    //scanner options
     var opts= {
         continuous: true,
         video: document.getElementById('preview'),
@@ -27,12 +34,15 @@ export function OpenScanner() {
         scanPeriod: 1
     };
 
+    
+//new instance scanner
     scanner = new Instascan.Scanner(opts);
 
-
+//all available cameras
     Instascan.Camera.getCameras().then(function(allcams){
     cameras = allcams;
-
+        
+//if no camers found alert appears "No ccameras found"
     if(cameras.length === 0){
         alert("No cameras found");
         return;
@@ -45,7 +55,7 @@ export function OpenScanner() {
         console.error(e);
     });
 
-
+//event listener whenthe camera scans the qr code
     scanner.addListener('scan', function(Code){
         document.getElementById("camera-scan-result").innerHTML = Code;
     });
@@ -56,20 +66,22 @@ export function ChangeCamera() {
     if(scanner) {
         scanner.stop();
 
-        // move to next camera
+        //move to next camera
         currentCamera = (currentCamera + 1) % cameras.length;
 
-        // start new camera
+        //start new camera
         scanner.start(cameras[currentCamera]).catch(e => {
             console.error("Camera switch failed:", e);
         });
 
+    // scan listener
         scanner.addListener('scan',function(Code){
             document.getElementById("camera-scan-result").innerHTML = Code;
     });
     }
 }
 
+//closes the scanner and hides the preview
 export function CloseScanner() {
     if (scanner) {
         scanner.stop();
