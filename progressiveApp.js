0db9020bf3c96bc56efd4f6a6c63a138f37ab971
PabloM50 <pmasliuk@uclan.ac.uk>
2026-03-31 15:29:49 +0300

    proggressive web app fix

--- progressiveApp.js
+++ progressiveApp.js
@@ -1,31 +1,9 @@
 const cacheName="Treasure-Hunt-v1";
 
 const fileToCache=[
-    "/CO1111/",
+    "/",
     "index.html",
     "app.html",
-    "Quiz.css",
-    "Animation.css",
-    "main.css",
-    "Message.css",
-    "video.css",
-    "AfterQuestionAnims.js",
-    "App.js",
-    "AppState.js",
-    "BooleanQuestion.js",
-    "IntegerQuestion.js",
-    "MCQQuestion.js",
-    "NumericQuestion.js",
-    "Question.js",
-    "TextQuestion.js",
-    "LeaderBoardStage.js",
-    "ListStage.js",
-    "Stages.js",
-    "StartStage.js",
-    "ClearRenderer.js",
-    "Message.js",
-    "Utils.js",
-    "main.js",
 ];
 
 //Start the service worker and cache all of the app's content.
